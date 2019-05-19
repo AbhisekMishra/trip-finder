a82c3c090715cb00e76384db66b7220d25d1964c
abhisek.mishra <abhisek.mishra@xebia.com>
2019-05-19 10:57:04 +0400

    pound symbol in html

--- src/client/components/search-result/search-result.js
+++ src/client/components/search-result/search-result.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   Container, Row, Col, Spinner, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText
 } from 'reactstrap';
 import train from '../../images/train.svg';
 import bus from '../../images/bus.svg';
 import car from '../../images/car.svg';
+
+const calculateTotalCost = result => result.reduce((acc, val) => ({ cost: acc.cost + val.cost })).cost;
+
+const convertMinutesToHours = (minutes) => {
+  const h = Math.floor(minutes / 60);
+  const m = minutes % 60;
+  return `${h}:${m}`;
+};
+
+const calculateTotalDuration = (result) => {
+  let totalMinutes = 0;
+  result.forEach((r) => {
+    totalMinutes = totalMinutes + (parseInt(r.duration.h, 10) * 60) + parseInt(r.duration.m, 10);
+  });
+  return convertMinutesToHours(totalMinutes);
+};
 
 const SearchResult = ({ isSearching, result }) => (
   <Container>
@@ -17,8 +34,13 @@ const SearchResult = ({ isSearching, result }) => (
               <Col>
                 <ListGroupItemHeading>{`${r.from} to ${r.to}`}</ListGroupItemHeading>
                 <ListGroupItemText>
-                  <p>{`Duration: ${r.duration.h}:${r.duration.m}`}</p>
-                  <p>{`Cost: ${r.cost}`}</p>
+                  <p>{`Duration: ${r.duration.h}:${r.duration.m} hours`}</p>
+                  <p>
+                    {'Cost:'}
+                    {' '}
+                    &pound;
+                    {`${r.cost}`}
+                  </p>
                 </ListGroupItemText>
               </Col>
               <Col>
@@ -30,9 +52,25 @@ const SearchResult = ({ isSearching, result }) => (
 
           </ListGroupItem>
         ))}
+        {result.length > 0 && (
+        <ListGroupItem>
+          <p>{`Total duration: ${calculateTotalDuration(result)} hours`}</p>
+          <p>
+            {'Total cost:'}
+            {' '}
+            &pound;
+            {`${calculateTotalCost(result)}`}
+          </p>
+        </ListGroupItem>
+        )}
       </ListGroup>
     </Row>
   </Container>
 );
+
+SearchResult.propTypes = {
+  isSearching: PropTypes.bool.isRequired,
+  result: PropTypes.arrayOf.isRequired,
+};
 
 export default SearchResult;
